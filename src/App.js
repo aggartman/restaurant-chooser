44eda5e1f5043956385870cb7750fbe6428fa404
Austin Gartman <aggartman@gmail.com>
2022-07-24 18:10:28 -0500

    Refine functionality of basic program Add CSS base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
+import React from 'react'
 import './App.css'
-// import restaurantScraper from './restaurantScraper';
-// import RestaurantRoulette from './RestaurantRoulette';
-import Restaurant from './Restaurant';
 
 function App() {
   const restaurantList = [
@@ -12,31 +10,42 @@ function App() {
     {name : 'Nobu', cuisine : 'Japanese', price : '$$$$'},
   ]
   var choice = 0;
-  function spinRoulette(e) {
-    choice = Math.floor(Math.random() * restaurantList.length);
-    console.log(choice);
+  function randomChoice() {
+    choice = restaurantList[Math.floor(Math.random() * restaurantList.length)];
     return choice
+  }
+  function spinRoulette(e) {
+    choice = randomChoice();
+    let rouletteTitle = document.getElementById('rw1');
+    let rouletteCuisine = document.getElementById('rw2');
+    let roulettePrice = document.getElementById('rw3');
+
+    rouletteTitle.innerText = choice.name;
+    rouletteCuisine.innerText = choice.cuisine;
+    roulettePrice.innerText = choice.price;
   }
   return (
     <>
-      <h1 className='title'>
-        Restaurant Roulette
-      </h1>
-      <div className='about'>
-        <p>
-          Ever get stuck choosing a place to eat? 
-          Indecisive much? 
-        </p>
-        <p>
-          Well then spin the wheels and try your luck!
-        </p>
+      <div className='container1'>
+        <h1 className='title'>
+          Restaurant Roulette
+        </h1>
+        <div className='about'>
+          <p>
+            Ever get stuck choosing a place to eat? 
+            Indecisive much? 
+          </p>
+          <p>
+            Well then spin the wheels and try your luck!
+          </p>
+        </div>
       </div>
-      <button className='spin' onClick={spinRoulette}>Spin</button>
-      < Restaurant restaurant={restaurantList[choice]}/>
-      {/* <div className='randomChoice'>
-        <h3>{restaurantList[choice].name}</h3>
-        <p>{restaurantList[choice].cuisine} {restaurantList[choice].price}</p>
-      </div> */}
+      <div id='randomChoice'>
+        <h3 className='rouletteWheel' id='rw1'> </h3>
+        <p className='rouletteWheel' id='rw2'> </p>
+        <p className='rouletteWheel' id='rw3'> </p>
+        <button className='spin' onClick={spinRoulette}>Spin</button>
+      </div>
     </>
   );
 }
