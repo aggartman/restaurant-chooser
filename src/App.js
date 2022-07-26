56a4f8875fdbf4b032a111e511d67c4ee4d7b327
Austin Gartman <aggartman@gmail.com>
2022-07-26 09:25:45 -0500

    Removed React componets in favor of standard html

--- src/App.js
+++ src/App.js
@@ -24,6 +24,20 @@ function App() {
     rouletteCuisine.innerText = choice.cuisine;
     roulettePrice.innerText = choice.price;
   }
+  var lock1 = document.getElementById('lock1');
+  lock1.onclick = toggleLock('lock1');
+  var lock2 = document.getElementById('lock2');
+  lock2.onclick = toggleLock('lock2')
+  function toggleLock(id) {
+    var obj = document.getElementById(id);
+     if (obj.innerText === 'lock') {
+      obj.innerText = 'lock_open';
+      obj.classList.add('open');
+     } else {
+      obj.innerText = 'lock';
+      obj.classList.remove('open');
+     }
+  }
   return (
     <>
       <div className='container1'>
@@ -40,15 +54,37 @@ function App() {
           </p>
         </div>
       </div>
+      <div id='locks'>
+        {/* <div className='lockContain' id='lC1'>
+          <span class="material-symbols-outlined">
+            lock_open
+          </span>
+        </div>
+        <div className='lockContain' id='lC2'>
+          <span class="material-symbols-outlined">
+            lock
+          </span>
+        </div> */}
+      </div>
       <div id='randomChoice'>
-        <div className='square'>
-          <h3 className='rouletteWheel' id='rw1'> </h3>
+        <div className='lockContain' id='lC1'>
+          <span className="material-symbols-outlined open" id='lock1'>
+            lock_open
+          </span>
         </div>
-        <div className='square'>
-          <p className='rouletteWheel' id='rw2'> </p>
+        <div className='lockContain' id='lC2'>
+          <span className="material-symbols-outlined" id='lock2'>
+            lock
+          </span>
         </div>
-        <div className='square'>
-          <p className='rouletteWheel' id='rw3'> </p>
+        <div className='square' id='rw1'>
+          <h3 className='rouletteWheel'> </h3>
+        </div>
+        <div className='square' id='rw2'>
+          <p className='rouletteWheel'> </p>
+        </div>
+        <div className='square' id='rw3'>
+          <p className='rouletteWheel'> </p>
         </div>
         <button className='spin' onClick={spinRoulette}>Spin</button>
       </div>
