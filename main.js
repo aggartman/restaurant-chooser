99c66c4519a3caad4a0912cc8e441c1c895a680f
Austin Gartman <aggartman@gmail.com>
2022-07-28 07:58:23 -0500

    Refactor choice selector and catagory selectors

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ const restaurantList = [
   {name : 'Maggianos', cuisine : 'Italian', price : '$$$'}, 
   {name : 'Nobu', cuisine : 'Japanese', price : '$$$$'},
 ]
+function populateSelectors() {
+  var cuisineSelector = document.getElementById('cuisine');
+  var priceSelector = document.getElementById('price');
+  restaurantList.forEach( function(restaurant) {
+    let cuisineOption = document.createElement('option');
+    let priceOption = document.createElement('option');
+    cuisineOption.innerText = restaurant.cuisine;
+    priceOption.innerText = restaurant.price;
+    cuisineSelector.appendChild(cuisineOption);
+    priceSelector.appendChild(priceOption);
+  })
+}
+
 var spin = document.getElementById('spinBtn');
 spin.addEventListener('click', spinRoulette);
 
@@ -13,8 +26,54 @@ var lock2 = document.getElementById('lock2');
 lock1.addEventListener('click', toggleLock);
 lock2.addEventListener('click', toggleLock);
 
+function selectCategories(category) {
+  if (category === 'cuisine') {
+    var selectedCuisine = document.getElementById('cuisine');
+    var selectedItem = selectedCuisine.options[selectedCuisine.selectedIndex].value;
+    return selectedItem
+  } else if (category === 'price') {
+    var selectedPrice = document.getElementById('price');
+    var selectedItem2 = selectedPrice.options[selectedPrice.selectedIndex].value;
+    return selectedItem
+  } else if (category === 'both') {
+    selectedCuisine = document.getElementById('cuisine');
+    selectedPrice = document.getElementById('price');
+    selectedItem = selectedCuisine.options[selectedCuisine.selectedIndex].value;
+    var selectedItem2 = selectedPrice.options[selectedPrice.selectedIndex].value;
+    var selectedItems = [selectedItem, selectedItem2];
+    return selectedItems
+  }
+}
+
+  
+function filterList(id) { 
+  selectCategories(id);
+  var selectedList = [];
+  restaurantList.forEach(function(restaurant) {
+    if (id == 'cuisine') {
+      if (restaurant.cuisine === selectedItem) {
+        selectedList.push(restaurant);
+      } 
+    } else if (id == 'price') {
+      if (restaurant.price === selectedItem) {
+        selectedList.push(restaurant);
+      }
+    }
+  })
+  return selectedList
+}
 function randomChoice() {
-  choice = restaurantList[Math.floor(Math.random() * restaurantList.length)];
+  if (lock1.innerText === 'lock') {
+    var selectedList = filterList('cuisine');
+    choice = selectedList[Math.floor(Math.random() * selectedList.length)];
+  } else if (lock2.innerText === 'lock') {
+    var selectedList = filterList('price');
+    choice = selectedList[Math.floor(Math.random() * selectedList.length)];
+  } else if (lock1.innerText === 'lock' && lock2.innerText === 'lock') {
+    var selectedList = filterList('both');
+  }else {
+    choice = restaurantList[Math.floor(Math.random() * restaurantList.length)];
+  }
   return choice
 }
 function spinRoulette(e) {
@@ -36,3 +95,5 @@ function toggleLock(e) {
     e.target.classList.remove('open');
    }
 }
+
+populateSelectors();
